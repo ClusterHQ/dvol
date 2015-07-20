2622d3f444697e2634df0d6eeb92762b138466bc
Luke Marsden <luke@clusterhq.com>
2015-07-20 06:58:26 +0100

    rearrange files, add setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name="Voluminous",
+    packages=[
+        "voluminous",
+        #"unofficial_flocker_tools.txflocker"
+    ],
+    entry_points={
+        "console_scripts": [
+            "dvol = voluminous.dvol:main",
+            "dvol-docker-plugin = voluminous.plugin:main",
+        ],
+    },
+    version="0.1",
+    description="A prototype docker volume manager with git-like functionality.",
+    author="Luke Marsden",
+    author_email="luke@clusterhq.com",
+    url="https://github.com/ClusterHQ/voluminous",
+    install_requires=[
+        #"PyYAML>=3",
+        "Twisted>=14",
+        "treq>=14",
+        "pyasn1>=0.1",
+    ],
+)
