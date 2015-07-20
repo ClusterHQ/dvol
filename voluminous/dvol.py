7ff8018517d5f609b94f539806eef7a89a8f01f1
Luke Marsden <luke@clusterhq.com>
2015-07-20 05:27:29 -0400

    First passing test: create a blank directory

--- voluminous/dvol.py
+++ voluminous/dvol.py
@@ -4,3 +4,56 @@ dvol: voluminous for Docker; Voluminuous client.
 For the prototype, we can probably get away with manipulating directories
 directly.
 """
+
+from twisted.python.usage import Options, UsageError
+from twisted.python.filepath import FilePath
+
+
+class Voluminous(object):
+    def __init__(self, directory):
+        self._directory = FilePath(directory)
+
+    def createVolume(self, name):
+        # TODO raise if already exists
+        self._directory.child(name).makedirs()
+
+
+class CreateOptions(Options):
+    """
+    Create a volume.
+    """
+    def parseArgs(self, name):
+        self.name = name
+
+
+    def run(self, voluminous):
+        voluminous.createVolume(self.name)
+
+
+
+class VoluminousOptions(Options):
+    """
+    Voluminous volume manager.
+    """
+    optParameters = [
+        ["pool", "p", None, "The name of the directory to use"],
+        ]
+
+    subCommands = [
+        ["init", None, CreateOptions, "Create a volume and its default master branch"],
+        #["list-all-branches", None, ListVolumesOptions, "List all branches"],
+        #["branch", None, BranchOptions, "Create a branch"],
+        #["list-branches", None, ListBranchesOptions, "List branches for specific volume"],
+        #["delete-branch", None, DeleteBranchOptions, "Delete a branch"],
+        #["tag", None, TagOptions, "Create a tag"],
+        #["list-tags", None, ListTagsOptions, "List tags"],
+        #["push-branch", None, PushBranchOptions, "Push a branch to another pool"],
+        ]
+
+
+    def postOptions(self):
+        if self.subCommand is None:
+            return self.opt_help()
+        if self["pool"] is None:
+            raise UsageError("pool is required")
+        self.subOptions.run(Voluminous(self["pool"]))
--- voluminous/test_dvol.py
+++ voluminous/test_dvol.py
@@ -4,10 +4,14 @@ Tests for the Voluminous CLI.
 
 from twisted.trial.unittest import TestCase
 from voluminous.dvol import VoluminousOptions
+from twisted.python.filepath import FilePath
 
 class VoluminousTests(TestCase):
+    def setUp(self):
+        self.tmpdir = FilePath(self.mktemp())
+        self.tmpdir.makedirs()
+
     def test_create_volume(self):
         dvol = VoluminousOptions()
-        import pdb; pdb.set_trace()
-        dvol.parseArgs("create", "-p", self.tmpdir(), "foo")
-        dvol.run()
+        dvol.parseOptions(["-p", self.tmpdir.path, "init", "foo"])
+        self.assertTrue(self.tmpdir.child("foo").exists())
