2bd4b4aca2d9a8a25c5c88bbf65b0c75642fbe43
Luke Marsden <luke@clusterhq.com>
2015-10-26 18:44:57 +0000

    update integration tests

--- voluminous/test_plugin.py
+++ voluminous/test_plugin.py
@@ -2,28 +2,34 @@
 log of integration tests to write:
 
 command:
+    docker-compose up -d (in a directory with appropriate docker-compose.yml file)
+expected behaviour:
+    docker containers are started with dvol accordingly
+    XXX this doesn't seem to work at the moment
+
+command:
     docker run -ti --volume-driver dvol -v hello:/data busybox sh
 expected output:
     Error response from daemon: Voluminous 'hello2' does not exist, create it with: dvol init hello2
 
 command:
-    sudo dvol init hello2
+    dvol init hello2
 expected output:
     Created volume hello2
     Created branch hello2/master
 
 command:
-    sudo dvol commit ...
+    dvol commit ...
 expected behaviour:
     a container which only persists its in-memory state to disk occasionally (e.g. on shutdown) has correctly written out its state
 
 command:
-    sudo dvol rollback ...
+    dvol reset...
 expected behaviour:
     a container which caches disk state in memory has correctly updated its state (IOW, containers get restarted around rollbacks)
 
 command:
     run a container using a dvol volume
-    sudo dvol list
+    dvol list
     container names shows up in output
 """
