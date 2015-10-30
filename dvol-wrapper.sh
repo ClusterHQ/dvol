a62f5cc55b6f00f32c0327f75c16d922d3efaa9c
Luke Marsden <luke@clusterhq.com>
2015-10-30 10:54:51 -0700

    remove this, it's in install.sh now

--- dvol-wrapper.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-docker run --rm -ti --volumes-from dvol-volumes \
-    -v /run/docker/plugins:/run/docker/plugins \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    clusterhq/dvol dvol "$@"
