5128c0d823e2eb99503d55ca06d0a717974c5ded
Luke Marsden <luke@clusterhq.com>
2016-02-02 14:36:15 +0000

    This is how you can test some code sometimes.

---
+++ dvol.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+    "fmt"
+
+//    "github.com/clusterhq/dvol/dockercontainers"
+//    "github.com/clusterhq/dvol/plugin"
+)
+
+func main() {
+    hello()
+}
+
+func hello() {
+    fmt.Printf("Hello, world.\n")
+}
---
+++ dvol_test.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+    "testing"
+)
+
+func TestHello(t *testing.T) {
+    hello()
+    // Output: Hello, World?
+}
