2e3dbf84f7751b7b546d15d6a825ad4df6b44d66
Luke Marsden <luke@clusterhq.com>
2016-02-22 22:24:00 +0000

    Puzzling, can't see why this shows the flag but doesn't accept it.

--- cmd/root.go
+++ cmd/root.go
@@ -1,25 +1,47 @@
 package cmd
 import (
     "fmt"
+    "path/filepath"
+    "os"
     "github.com/spf13/cobra"
 )
 
-var path string
+var basePath string
+var echoTimes int
+var disableDockerIntegration bool
 
 var RootCmd = &cobra.Command {
     Use: "dvol",
     Short: "dvol is a version control system for your development data in Docker",
-    Long: `dvol lets you commit, reset and branch the containerized databases
+    Long: `dvol
+====
+dvol lets you commit, reset and branch the containerized databases
 running on your laptop so you can easily save a particular state
 and come back to it later.`,
-    Run: func(cmd *cobra.Command, args []string) {
-        fmt.Println("Hello, world! I am really here.", path)
-    },
 }
+
+var cmdTimes = &cobra.Command{
+        Use:   "init",
+        Short: "Create a volume and its default master branch, then switch to it",
+        Run: func(cmd *cobra.Command, args []string) {
+            if(len(args) == 0) {
+                fmt.Println("Please specify a volume name.")
+                return
+            }
+            fmt.Println("Initializing", args[0])
+            path := filepath.FromSlash(basePath + "/" + args[0]) // XXX SEC
+            os.MkdirAll(path, 0777)
+        },
+    }
 
 func init() {
     // cobra.OnInitialize(initConfig)
     // TODO support: dvol -p <custom_path> init <volume_name>
-    RootCmd.Flags().StringVarP(&path, "path", "p", "/var/lib/dvol/volumes",
+    RootCmd.AddCommand(cmdTimes)
+
+    RootCmd.Flags().StringVarP(&basePath, "path", "p", "/var/lib/dvol/volumes",
         "The name of the directory to use")
+    RootCmd.Flags().BoolVar(&disableDockerIntegration,
+        "disable-docker-integration", false, "Do not attempt to list/stop/start" +
+        " docker containers which are using dvol volumes")
 }
