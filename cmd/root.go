07f8814572d91780564b544ad4af5ed37c950eb5
Luke Marsden <luke@clusterhq.com>
2016-02-23 11:42:40 +0000

    WIP on data layer APIs

--- cmd/root.go
+++ cmd/root.go
@@ -4,6 +4,7 @@ import (
     "path/filepath"
     "os"
     "github.com/spf13/cobra"
+    "../datalayer"
 )
 
 var basePath string
@@ -20,21 +21,44 @@ running on your laptop so you can easily save a particular state
 and come back to it later.`,
 }
 
+DEFAULT_BRANCH := "master"
+
+            // TODO Factor this into a data layer object.
+            os.MkdirAll(filepath.FromSlash(
+                basePath + "/" + volumeName), 0777) // XXX SEC
+            os.MkdirAll(filepath.FromSlash(
+                basePath + "/" + volumeName + "/branches/master"), 0777) // XXX SEC
+
 var cmdTimes = &cobra.Command{
         Use:   "init",
         Short: "Create a volume and its default master branch, then switch to it",
         Run: func(cmd *cobra.Command, args []string) {
             if(len(args) == 0) {
                 fmt.Println("Please specify a volume name.")
-                return
+                os.Exit(1)
             }
-            fmt.Println("Created volume", args[0])
-            fmt.Println("Created branch " + args[0] + "/master")
-            // TODO Factor this into a data layer object.
-            os.MkdirAll(filepath.FromSlash(
-                basePath + "/" + args[0]), 0777) // XXX SEC
-            os.MkdirAll(filepath.FromSlash(
-                basePath + "/" + args[0] + "/branches/master"), 0777) // XXX SEC
+            volumeName = args[0]
+            if(!datalayer.ValidVolumeName(volumeName)) {
+                fmt.Println("Error: " + volumeName + " is not a valid name")
+                os.Exit(1)
+            }
+            if datalayer.VolumeExists(volumeName) {
+                fmt.Println("Error: volume " + volumeName + " already exists")
+                os.Exit(1)
+            }
+            err := datalayer.CreateVolume(volumeName)
+            if(err) {
+                fmt.Println("Error creating volume")
+                os.Exit(1)
+            }
+            fmt.Println("Created volume", volumeName)
+
+            err := datalayer.CreateVariant(volumeName, "master")
+            if(err) {
+                fmt.Println("Error creating branch")
+                os.Exit(1)
+            }
+            fmt.Println("Created branch " + volumeName + "/master")
         },
     }
 
