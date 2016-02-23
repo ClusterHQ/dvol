38655cf0b1c50303fa1d7ee059ea514a803d8ffb
Bridget McErlean <bridgetmcerlean@gmail.com>
2016-02-23 18:11:19 +0000

    Move directory creation into the datalayer package

--- datalayer/main.go
+++ datalayer/main.go
@@ -1,30 +1,35 @@
 package datalayer
 
 import (
-    "regexp"
+	"os"
+	"path/filepath"
+	"regexp"
 )
+
 const MAX_VOLUME_NAME_LENGTH int = 40
+
 // ClusterHQ data layer, naive vfs (directory-based) implementation
 
 func ValidVolumeName(volumeName string) bool {
-    var validVolumeRegex = regexp.MustCompile(`^[a-zA-Z]+[a-zA-Z0-9-]*$`)
-    return validVolumeRegex.MatchString(volumeName) && len(volumeName) <= MAX_VOLUME_NAME_LENGTH
+	var validVolumeRegex = regexp.MustCompile(`^[a-zA-Z]+[a-zA-Z0-9-]*$`)
+	return validVolumeRegex.MatchString(volumeName) && len(volumeName) <= MAX_VOLUME_NAME_LENGTH
 }
 
-func VolumeExists(volumeName string) bool {
-    return true
+func VolumeExists(basePath string, volumeName string) bool {
+	volumePath := filepath.FromSlash(basePath + "/" + volumeName)
+	_, err := os.Stat(volumePath)
+	return err == nil
 }
 
-func CreateVolume(volumeName string) error {
-
-            // TODO Factor this into a data layer object.
-            //os.MkdirAll(filepath.FromSlash(
-            //    basePath + "/" + volumeName), 0777) // XXX SEC
-    return nil
+func CreateVolume(basePath string, volumeName string) error {
+	volumePath := filepath.FromSlash(basePath + "/" + volumeName)
+	// TODO Factor this into a data layer object.
+	os.MkdirAll(volumePath, 0777) // XXX SEC
+	return nil
 }
-func CreateVariant(volumeName, variantName string) error {
-    // XXX Variants are meant to be tagged commits???
-            //os.MkdirAll(filepath.FromSlash(
-            //    basePath + "/" + volumeName + "/branches/master"), 0777) // XXX SEC
-    return nil
+func CreateVariant(basePath, volumeName, variantName string) error {
+	// XXX Variants are meant to be tagged commits???
+	variantPath := filepath.FromSlash(basePath + "/" + volumeName + "/branches/master")
+	os.MkdirAll(variantPath, 0777) // XXX SEC
+	return nil
 }
