c958d5a9f019d58e2b3fcb68a228476caefcade0
Mark deVilliers <markdevilliers@gmail.com>
2016-02-24 06:46:33 +0000

    predicate files not in vendor directory, only do one pass of testing

--- scripts/run-preflight.sh
+++ scripts/run-preflight.sh
@@ -11,17 +11,15 @@ test -z "$(goimports -w .    | tee /dev/stderr)"
 echo "running go vet..."
 godep go vet ./...
 
+# run test coverage on each subdirectories and merge the coverage profile.
 echo "running go test..."
-godep go test -race ./...
-
-# run test coverage on each subdirectories and merge the coverage profile. 
-echo "mode: count" > profile.cov
  
+echo "mode: count" > profile.cov 
 # standard go tooling behavior is to ignore dirs with leading underscors
-for dir in $(find . -maxdepth 10 -not -path './.git*' -not -path '*/_*' -type d);
+for dir in $(find . -maxdepth 10 -not -path './.git*' -not -path '*/vendor/*' -not -path '*/_*' -type d);
 do
 if ls $dir/*.go &> /dev/null; then
-    godep go test -covermode=count -coverprofile=$dir/profile.tmp $dir
+    godep go test -race -covermode=count -coverprofile=$dir/profile.tmp $dir
     if [ -f $dir/profile.tmp ] 
     then
         cat $dir/profile.tmp | tail -n +2 >> profile.cov
