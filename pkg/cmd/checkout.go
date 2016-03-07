f239a5fb48058f0cd7796f2c5f0a1f87c31b6d82
Brendan Donegan <brendan.donegan@clusterhq.com>
2016-03-07 11:55:19 +0000

    Shell of checkout command

---
+++ pkg/cmd/checkout.go
@@ -0,0 +1,29 @@
+package cmd
+
+func NewCmdCheckout(out io.Writer) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "checkout",
+		Short: "Check out or create branches on the active volume",
+		Run: func(cmd *cobra.Command, args []string) {
+			err := checkoutBranch(cmd, args, out)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err.Error())
+				os.Exit(1)
+			}
+		},
+	}
+	cmd.Flags().BoolVarP(&createBranch, "branch", "b", false, "Create branch")
+	return cmd
+}
+
+func checkoutBranch(cmd *cobra.Command, args []string, out io.Writer) error {
+	err := checkBranchArgs(args)
+	if err != nil {
+		return err
+	}
+	branchName := args[0]
+	if !datalayer.ValidName(branchName) {
+		return fmt.Errorf("Error: " + branchName + " is not a valid name")
+	}
+	return nil
+}
