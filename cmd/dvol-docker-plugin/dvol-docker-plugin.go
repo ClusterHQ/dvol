f50c22c110e0c31fb22e4547b7f9644a20ce5d88
Rob Haswell <me@robhaswell.co.uk>
2016-03-09 16:55:22 +0000

    Create the volume directory if it does not exist.

--- cmd/dvol-docker-plugin/dvol-docker-plugin.go
+++ cmd/dvol-docker-plugin/dvol-docker-plugin.go
@@ -13,7 +13,9 @@ import (
 	"github.com/ClusterHQ/dvol/pkg/api"
 )
 
-const DVOL_SOCKET = "/run/docker/plugins/dvol.sock"
+const PLUGINS_DIR = "/run/docker/plugins"
+const DVOL_SOCKET = PLUGINS_DIR + "/dvol.sock"
+const VOL_DIR = "/var/lib/dvol/volumes"
 const DVOL_BASE_DIR = "/var/run/dvol"
 
 type ResponseImplements struct {
@@ -32,6 +34,11 @@ type RequestMount struct {
     Name string
 }
 
+type RequestRemove struct {
+    // A request to remove a volume for Docker
+    Name string
+}
+
 type ResponseSimple struct {
     // A response which only indicates if there was an error or not
     Err string
@@ -44,14 +51,26 @@ type ResponseMount struct {
 }
 
 func main () {
-    if _, err := os.Stat(DVOL_SOCKET); err == nil {
-        if err = os.Remove(DVOL_SOCKET); err == nil {
-            log.Fatalf("Could not clean up existing socket at %s", DVOL_SOCKET)
+    if _, err := os.Stat(PLUGINS_DIR); err != nil {
+        if err := os.MkdirAll(PLUGINS_DIR, 0700); err != nil {
+            log.Fatalf("Could not make plugin directory %s: %v", PLUGINS_DIR, err)
         }
     }
+    if _, err := os.Stat(DVOL_SOCKET); err == nil {
+        if err = os.Remove(DVOL_SOCKET); err != nil {
+            log.Fatalf("Could not clean up existing socket at %s: %v", DVOL_SOCKET, err)
+        }
+    }
+    if _, err := os.Stat(VOL_DIR); err != nil {
+        if err := os.MkdirAll(VOL_DIR, 0700); err != nil {
+            log.Fatalf("Could not make volumes directory %s: %v", VOL_DIR, err)
+        }
+    }
+
     listener, err := net.Listen("unix", DVOL_SOCKET)
+
     if err != nil {
-        log.Fatalf("Could not listen on %s", DVOL_SOCKET)
+        log.Fatalf("Could not listen on %s: %v", DVOL_SOCKET, err)
     }
 
     http.HandleFunc("/Plugin.Activate", func(w http.ResponseWriter, r *http.Request) {
@@ -80,6 +99,26 @@ func main () {
     })
 
     http.HandleFunc("/VolumeDriver.Remove", func(w http.ResponseWriter, r *http.Request) {
+        log.Print("<= /VolumeDriver.Remove")
+        requestJSON, err := ioutil.ReadAll(r.Body)
+        if err != nil {
+            log.Fatalf("Unable to read response body %s", err)
+        }
+        request := new(RequestRemove)
+        json.Unmarshal(requestJSON, request)
+        name := request.Name
+        dvol := api.NewDvolAPI(DVOL_BASE_DIR)
+        if dvol.VolumeExists(name) {
+            err = dvol.RemoveVolume(name)
+            if err != nil {
+                WriteResponseErr(err, w)
+            } else {
+                WriteResponseOK(w)
+            }
+        } else {
+            log.Printf("Requested to remove unknown volume %s", name)
+            WriteResponseErr(errors.New("Requested to remove unknown volume " + name), w)
+        }
     })
 
     http.HandleFunc("/VolumeDriver.Path", func(w http.ResponseWriter, r *http.Request) {
@@ -96,29 +135,26 @@ func main () {
         name := request.Name
 
         dvol := api.NewDvolAPI(DVOL_BASE_DIR)
-        var mountpoint string
 
         if dvol.VolumeExists(name) {
             err := dvol.SwitchVolume(name)
             if err != nil {
                 WriteResponseErr(err, w)
-                return
             }
-            mountpoint, err = dvol.ActiveVolume()
+            _, err = dvol.ActiveVolume()
             if err != nil {
                 WriteResponseErr(err, w)
-                return
             }
+            // mountpoint should be:
+            // /var/lib/docker/volumes/<volumename>/running_point
+            responseJSON, _ := json.Marshal(&ResponseMount{
+                Mountpoint: "/tmp", // TODO: Get the real path
+                Err: "",
+            })
+            w.Write(responseJSON)
         } else {
             WriteResponseErr(errors.New("Requested to mount unknown volume " + name), w)
-            return
         }
-
-        responseJSON, _ := json.Marshal(&ResponseMount{
-            Mountpoint: mountpoint,
-            Err: "",
-        })
-        w.Write(responseJSON)
     })
 
     http.HandleFunc("/VolumeDriver.Unmount", func(w http.ResponseWriter, r *http.Request) {
