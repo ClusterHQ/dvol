f3e4c6175ba938026ee9d30cf239b0ff657dfc1e
Mark deVilliers <markdevilliers@gmail.com>
2016-03-10 16:03:38 +0000

    force remove the package

--- scripts/build-install-docker.sh
+++ scripts/build-install-docker.sh
@@ -20,7 +20,7 @@ if [ $DOCKER_VERSION = "experimental" ]; then
 
   # We need to remove the existing version as we cannot add --force-confnew
   # to the apt-get install in the following call
-  sudo apt-get --force-yes remove docker-engine
+  sudo apt-get --force-yes -y -q remove docker-engine
 
   # Install the 'experimental' build from Docker
   curl -sSL https://experimental.docker.com/ | sudo sh
