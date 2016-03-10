9c0dc009c5ba2baf3811f1016f3dab0589e97017
Mark deVilliers <markdevilliers@gmail.com>
2016-03-10 14:17:20 +0000

    add ability to install the experimental Docker build

---
+++ scripts/build-install-docker.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Installs either a known version of Docker or the 'experimental' build.
+
+set -xe
+
+# exit if DOCKER_VERSION is not set
+if [ -z "$DOCKER_VERSION" ]; then
+  exit 0
+fi
+
+if [ $DOCKER_VERSION = "experimental" ]; then
+  # 'upgrade' docker-engine to specific version
+  apt-get -o Dpkg::Options::="--force-confnew" install -y --force-yes docker-engine=${DOCKER_VERSION}-0~trusty
+else
+  # install the 'experimental' build from Docker
+  curl -sSL https://experimental.docker.com/ | sh
+fi
