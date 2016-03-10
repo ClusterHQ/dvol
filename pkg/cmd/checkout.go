03d6d1f0819714ef3f676db24605136c75b04d7d
Brendan Donegan <brendan.donegan@clusterhq.com>
2016-03-10 13:11:05 +0000

    Get code to the point where we can run the tests.

--- pkg/cmd/checkout.go
+++ pkg/cmd/checkout.go
@@ -1,11 +1,11 @@
 package cmd
 
 import (
-	"fmt"
+	//"fmt"
 	"io"
-	"os"
+	//"os"
 
-	"github.com/ClusterHQ/dvol/pkg/api"
+	//"github.com/ClusterHQ/dvol/pkg/api"
 	"github.com/spf13/cobra"
 )
 
@@ -16,17 +16,18 @@ func NewCmdCheckout(out io.Writer) *cobra.Command {
 		Use:   "checkout",
 		Short: "Check out or create branches on the active volume",
 		Run: func(cmd *cobra.Command, args []string) {
-			err := checkoutBranch(cmd, args, out)
+			/*err := checkoutBranch(cmd, args, out)
 			if err != nil {
 				fmt.Fprintln(os.Stderr, err.Error())
 				os.Exit(1)
-			}
+			}*/
 		},
 	}
 	cmd.Flags().BoolVarP(&createBranch, "branch", "b", false, "Create branch")
 	return cmd
 }
 
+/*
 func checkoutBranch(cmd *cobra.Command, args []string, out io.Writer) error {
 	dvol := api.NewDvolAPI(basePath)
 	err := checkBranchArgs(args)
@@ -41,3 +42,4 @@ func checkoutBranch(cmd *cobra.Command, args []string, out io.Writer) error {
 	fmt.Printf("%s: %s\n", branchName, err)
 	return dvol.CheckoutBranch(branchName)
 }
+*/
