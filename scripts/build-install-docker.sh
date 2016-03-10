18d5b663ad8f9fffeb3255b49713461fec64aa89
Mark deVilliers <markdevilliers@gmail.com>
2016-03-10 15:12:17 +0000

    execute experimental script as sudo

--- scripts/build-install-docker.sh
+++ scripts/build-install-docker.sh
@@ -18,7 +18,7 @@ DOCKER_VERSION=$1
 
 if [ $DOCKER_VERSION = "experimental" ]; then
   # Install the 'experimental' build from Docker
-  sudo sh -c 'curl -sSL https://experimental.docker.com/'
+  curl -sSL https://experimental.docker.com/ | sudo sh
 else
   # 'upgrade' docker-engine to specific version
   sudo apt-get -o Dpkg::Options::="--force-confnew" install -y --force-yes docker-engine=${DOCKER_VERSION}-0~trusty
