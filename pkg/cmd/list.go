693fb19bee3cebf83b21d1dd05755b2916ca68d2
Bridget McErlean <bridgetmcerlean@gmail.com>
2016-03-12 21:52:18 +0000

    Use volume.Name

--- pkg/cmd/list.go
+++ pkg/cmd/list.go
@@ -60,7 +60,7 @@ func listVolumes(cmd *cobra.Command, args []string, out io.Writer) error {
 		if err != nil {
 			return err
 		}
-		containers, err := dvol.Containers.Related(volume)
+		containers, err := dvol.Containers.Related(volume.Name)
 		cStr := strings.Join(containers, ",")
 		if err != nil {
 			return err
