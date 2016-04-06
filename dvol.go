f8b3fe7892e6aa9d07753dfbd590847d0931168f
Rob Haswell <me@robhaswell.co.uk>
2016-04-06 12:33:14 +0100

    Don't double-print the error message.

--- dvol.go
+++ dvol.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/ClusterHQ/dvol/pkg/cmd"
@@ -9,7 +8,6 @@ import (
 
 func main() {
 	if err := cmd.RootCmd.Execute(); err != nil {
-		fmt.Fprintln(os.Stderr, err)
 		os.Exit(-1)
 	}
 }
